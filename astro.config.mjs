459538df971d33f79864e4f9e3e8efc6ee247926
Amebus <federico.ceruti@gmail.com>
2026-04-04 15:19:32 +0000

    project startup

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel';
+import bun from "@nurodev/astro-bun";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  integrations: [react()],
+  adapter: bun(),
+  // adapter: vercel(),
+});
